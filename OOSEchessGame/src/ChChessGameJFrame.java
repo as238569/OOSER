1d4c301ff746013a1ff0a352bbc78dd6c1f15dfb
ZHANG,BO-ZHI <as238569@gmail.com>
2013-12-25 04:27:38 +0800

    Some Change

--- OOSEchessGame/src/ChChessGameJFrame.java
+++ OOSEchessGame/src/ChChessGameJFrame.java
@@ -6,19 +6,13 @@ import javax.swing.ScrollPaneConstants;
 public class ChChessGameJFrame extends ChessGameJFrame{
 
     public ChChessGameJFrame(String title, ChessGame ObservableChessGame) {
-		super(title, ObservableChessGame);
-		// TODO Auto-generated constructor stub
-		
+    	
+		super(title, ObservableChessGame);		
 		boardJPanel =new ChessBoardJPanel(ObservableChessGame.getChessBoard());
   	    boardJPanel.setBounds(343, 90, 612, 680);
 	    getContentPane().add(boardJPanel);	
 	    
-	    //¤å¦r®Ø
-	    JTextArea record = new JTextArea();
-    	JScrollPane scroll = new JScrollPane(record);
-    	scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-    	getContentPane().add(scroll);
-    	scroll.setBounds(1000, 90, 200, 680);
+	    
 	}
 
 }
--- OOSEchessGame/src/ChessGameJFrame.java
+++ OOSEchessGame/src/ChessGameJFrame.java
@@ -31,25 +31,36 @@ public class ChessGameJFrame extends JFrame  implements  Observer{
     	
 	    
 	    /*--------------------------------------------------*/
+    	//回合標記
 	    MarkerJLabelR = new RoundMarkerJLabel(ObservableChessGame.getPlayer1(),"Up");
 	    MarkerJLabelR.setBounds(785, 11, 70, 70);
 	    getContentPane().add(MarkerJLabelR);
 	    
+	    //回合標記
 	    MarkerJLabelB = new RoundMarkerJLabel(ObservableChessGame.getPlayer2(),"Down");
 	    MarkerJLabelB.setBounds(477, 781, 70, 70);
 	    getContentPane().add(MarkerJLabelB);
 	    
+	    //玩家姓名
 	    nameJLabelR = new JLabel( ObservableChessGame.getPlayer1().getName());
 	    nameJLabelR.setBounds(890, 25, 100, 20);
 	    nameJLabelR.setBackground(Color.WHITE);
 	    nameJLabelR.setOpaque(true);
 	    getContentPane().add( nameJLabelR);
 	    
+	  //玩家姓名
 	    nameJLabelB = new JLabel(ObservableChessGame.getPlayer2().getName());
 	    nameJLabelB.setBounds(343, 791, 100, 20);
 	    nameJLabelB.setBackground(Color.WHITE);
 	    nameJLabelB.setOpaque(true);
 	    getContentPane().add(nameJLabelB);
+	    
+	    //文字框
+	    JTextArea record = new JTextArea();
+    	JScrollPane scroll = new JScrollPane(record);
+    	scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    	getContentPane().add(scroll);
+    	scroll.setBounds(1100, 90, 200, 680);
 		/*----------------------------------------------------*/		
 	}     
      
